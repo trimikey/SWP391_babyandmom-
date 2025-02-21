548d33fc4be045f2f2ed2fa7050a2f2e2d8f4405
huytb-6204 <huytran62044@gmail.com>
2025-02-21 15:35:00 +0700

    CRUD Membership packages and changePassord

--- src/main/java/com/swp/BabyandMom/Controller/UserController.java
+++ src/main/java/com/swp/BabyandMom/Controller/UserController.java
@@ -1,5 +1,7 @@
 package com.swp.BabyandMom.Controller;
 
+import com.swp.BabyandMom.DTO.ChangePasswordRequestDTO;
+import com.swp.BabyandMom.DTO.ChangePasswordResponseDTO;
 import com.swp.BabyandMom.DTO.GetProfileResponseDTO;
 import com.swp.BabyandMom.DTO.UpdateProfileRequestDTO;
 import com.swp.BabyandMom.DTO.UpdateProfileResponseDTO;
@@ -8,9 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 @RestController
 @RequestMapping("/api")
+@SecurityRequirement(name = "api")  
 @CrossOrigin("*")
 
 public class UserController {
@@ -24,5 +27,10 @@ public class UserController {
     @PutMapping("/update-profile")
     public UpdateProfileResponseDTO updateProfile(@RequestBody UpdateProfileRequestDTO updateRequestDTO) throws Exception {
         return userService.update(updateRequestDTO);
+    }
+
+    @PutMapping("/change-password")
+    public ResponseEntity<ChangePasswordResponseDTO> changePassword(@RequestBody ChangePasswordRequestDTO request) {
+        return userService.changePassword(request);
     }
 }

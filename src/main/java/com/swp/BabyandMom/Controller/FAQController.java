7009011c16acefe3e7cf4345fb15657a12977e9e
margaretmini <dominhchau2405@gmail.com>
2025-02-21 14:43:14 +0700

    Commit before pull code

--- src/main/java/com/swp/BabyandMom/Controller/FAQController.java
+++ src/main/java/com/swp/BabyandMom/Controller/FAQController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/faqs")
+@RequestMapping("/api/faqs")
 public class FAQController {
     private final FAQService faqService;
 

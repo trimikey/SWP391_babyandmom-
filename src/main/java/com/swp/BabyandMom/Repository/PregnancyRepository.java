435f89120f4d4296ec3dec5182eb5db499ff0711
margaretmini <dominhchau2405@gmail.com>
2025-03-06 13:21:01 +0700

    feat: getPregnancyProfileById

--- src/main/java/com/swp/BabyandMom/Repository/PregnancyRepository.java
+++ src/main/java/com/swp/BabyandMom/Repository/PregnancyRepository.java
@@ -5,11 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Repository
 public interface PregnancyRepository extends JpaRepository<Pregnancy_Profile, Long> {
     List<Pregnancy_Profile> findByUser(User user);
     List<Pregnancy_Profile> findByUserAndIsDeletedFalse(User user);
+    Optional<Pregnancy_Profile> findByIdAndUserAndIsDeletedFalse(Long id, User user);
+
 }
 

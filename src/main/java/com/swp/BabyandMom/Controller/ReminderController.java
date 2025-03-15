2d8a4738f9f87b4db18fad36538acbfdae45fc37
margaretmini <dominhchau2405@gmail.com>
2025-03-15 13:59:59 +0700

    fix reminder

--- src/main/java/com/swp/BabyandMom/Controller/ReminderController.java
+++ src/main/java/com/swp/BabyandMom/Controller/ReminderController.java
@@ -2,8 +2,9 @@ package com.swp.BabyandMom.Controller;
 
 import com.swp.BabyandMom.DTO.ReminderRequestDTO;
 import com.swp.BabyandMom.DTO.ReminderResponseDTO;
-import com.swp.BabyandMom.Entity.Enum.ReminderType;
+import com.swp.BabyandMom.Entity.Enum.MembershipType;
 import com.swp.BabyandMom.Service.ReminderService;
+import com.swp.BabyandMom.Utils.UserUtils;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,37 +18,43 @@ import java.util.List;
 @SecurityRequirement(name = "bearerAuth")
 public class ReminderController {
     private final ReminderService service;
+    private final UserUtils userUtils;
+
+    private void checkPremiumUser() {
+        MembershipType membershipType = userUtils.getUserMembershipType();
+        if (membershipType != MembershipType.PREMIUM) {
+            throw new RuntimeException("Access denied: Only PREMIUM users can access this feature.");
+        }
+    }
 
     @GetMapping
     public List<ReminderResponseDTO> getAllReminders() {
+        checkPremiumUser();
         return service.getAllReminders();
     }
 
-    @GetMapping("/enum/types")
-    public ResponseEntity<ReminderType[]> getReminderTypes() {
-        return ResponseEntity.ok(ReminderType.values());
-    }
-
-
     @GetMapping("/{id}")
     public ResponseEntity<ReminderResponseDTO> getReminderById(@PathVariable Long id) {
+        checkPremiumUser();
         return ResponseEntity.ok(service.getReminderById(id));
     }
 
     @PostMapping
     public ReminderResponseDTO createReminder(@RequestBody ReminderRequestDTO request) {
+        checkPremiumUser();
         return service.createReminder(request);
     }
 
     @PutMapping("/{id}")
     public ReminderResponseDTO updateReminder(@PathVariable Long id, @RequestBody ReminderRequestDTO request) {
+        checkPremiumUser();
         return service.updateReminder(id, request);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteReminder(@PathVariable Long id) {
+        checkPremiumUser();
         service.deleteReminder(id);
         return ResponseEntity.ok("Delete successfully");
     }
-
 }
